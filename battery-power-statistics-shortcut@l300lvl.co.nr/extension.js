f5c92bf71d73f8cb1a5e01be4c89ac67fb6cb348
l300lvl <atz3@yahoo.ca>
2012-09-07 23:00:46 -0500

    adds system-monitor compatability

--- battery-power-statistics-shortcut@l300lvl.co.nr/extension.js
+++ battery-power-statistics-shortcut@l300lvl.co.nr/extension.js
@@ -14,13 +14,27 @@ function _onPowerStatsActivate() {
 }
 
 function init() {
-    battery = Main.panel._statusArea.battery.menu;
+// if function burrowed from https://github.com/Mystro256/gnome-shell-extension-inhibit-applet/commit/58c06b7ab2f608167f4ede4b847681bd7586d016
+//    battery = Main.panel._statusArea.battery.menu;
+
+//    battery = Main.panel._statusArea.battery;
+//	    let temp = Main.panel._statusArea.battery._deviceItems;
+//	    if(temp == "")
+//	    {//check for no battery or power device, i.e. no power menu
+//	        lettemp = new Main.Panel.STANDARD_STATUS_AREA_SHELL_IMPLEMENTATION["a11y"]
+	        if (Main.panel._statusArea.battery != null) {
+	            battery = Main.panel._statusArea.battery.menu;
+	        } else {
+	            battery = Main.panel._statusArea.systemMonitor.menu;
+	        }
+//	    }
 }
 
 function enable() {
     item = new PopupMenu.PopupMenuItem(_("Power Statistics"));
     item.connect('activate', Lang.bind(item, _onPowerStatsActivate));
-    battery.addMenuItem(item, 3);
+    let nItems = battery.numMenuItems;
+    battery.addMenuItem(item, nItems - 1);
 }
 
 function disable() {
