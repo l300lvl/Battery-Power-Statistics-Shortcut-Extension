038d11498674eb2fbe3baa216747edcbc8d92aa7
l300lvl <atz3@yahoo.ca>
2012-10-06 03:07:58 -0500

    3.6 patch but not working with sysmon

---
+++ README.md
@@ -0,0 +1,17 @@
+### Description: 
+
+Adds a shortcut to open Power Statistics from the battery indicator. Incredibly useful for laptop users. 
+
+Now compatible with the system-monitor extension http://tinyurl.com/sysmon That is, if you use the system-monitor extension and chose to hide the system battery icon in it's preferences.
+
+Depends on: gnome-power-statistics
+
+
+### 1 click install from E.G.O:
+
+https://extensions.gnome.org/extension/175/battery-power-statistics-shortcut/
+
+
+### Screenshot
+
+![Screenshot](https://raw.github.com/l300lvl/Battery-Power-Statistics-Shortcut-Extension/master/screenshot.png)
--- battery-power-statistics-shortcut@l300lvl.co.nr/extension.js
+++ battery-power-statistics-shortcut@l300lvl.co.nr/extension.js
@@ -1,5 +1,6 @@
 
 const St = imports.gi.St;
+const Config = imports.misc.config;
 const Main = imports.ui.main;
 const Lang = imports.lang;
 const Shell = imports.gi.Shell;
@@ -13,23 +14,6 @@ function _onPowerStatsActivate() {
     app.activate();
 }
 
-function init() {
-// if function burrowed from https://github.com/Mystro256/gnome-shell-extension-inhibit-applet/commit/58c06b7ab2f608167f4ede4b847681bd7586d016
-//    battery = Main.panel._statusArea.battery.menu;
-
-//    battery = Main.panel._statusArea.battery;
-//	    let temp = Main.panel._statusArea.battery._deviceItems;
-//	    if(temp == "")
-//	    {//check for no battery or power device, i.e. no power menu
-//	        lettemp = new Main.Panel.STANDARD_STATUS_AREA_SHELL_IMPLEMENTATION["a11y"]
-	        if (Main.panel._statusArea.battery != null) {
-	            battery = Main.panel._statusArea.battery.menu;
-	        } else {
-	            battery = Main.panel._statusArea.systemMonitor.menu;
-	        }
-//	    }
-}
-
 function enable() {
     item = new PopupMenu.PopupMenuItem(_("Power Statistics"));
     item.connect('activate', Lang.bind(item, _onPowerStatsActivate));
@@ -40,5 +24,38 @@ function enable() {
 function disable() {
     if (item) {
         item.destroy();
+    }
+}
+
+
+let age, battMenu;
+
+function init() {
+    let current_version = Config.PACKAGE_VERSION.split('.');
+    if (current_version.length != 3 || current_version[0] != 3) throw new Error("Strange version number (extension.js:35).");
+    
+    switch (current_version[1]) {
+        case"2": global.log("Warning of extension [" + metadata.uuid + "]:\n              Old development release detected (" + Config.PACKAGE_VERSION + "). You should upgrade!\n");   //eak
+        case"3":  ;
+        case"4": age = "old";
+            break;
+        case"5": global.log("Warning of extension [" + metadata.uuid + "]:\n              Development release detected (" + Config.PACKAGE_VERSION + "). Loading as a 3.6 release.\n"); //eak
+        case"6": age = "new";
+            break;
+        default: throw new Error("Strange version number (extension.js:45).");
+    }
+
+    if (age=="old") battMenu = Main.panel._statusArea;
+    else            battMenu = Main.panel.statusArea;
+
+// if function burrowed from https://github.com/Mystro256/gnome-shell-extension-inhibit-applet/commit/58c06b7ab2f608167f4ede4b847681bd7586d016
+    if (battMenu.battery != null) {
+        battery = battMenu.battery.menu;
+	} else if (battMenu.systemMonitor != null) {
+	battery = battMenu.systemMonitor.menu;
+	} else if (battMenu.system-monitor != null) {
+	battery = battMenu.system-monitor.menu;
+	} else {
+	battery = battMenu.userMenu.menu;
     }
 }
